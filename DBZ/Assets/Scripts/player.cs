c058bceba73ec5c15d4289acd748acd9eb8e1dd2
Unknown <jerome.papalie@gmail.com>
2018-12-02 14:27:51 +0800

    character Jumps WIP

--- DBZ/Assets/Scripts/player.cs
+++ DBZ/Assets/Scripts/player.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class player : MonoBehaviour {
 
-	public float;
+	public float moveSpeed;
+	public float jumpPower;
+
+	private Rigidbody2D _player;
 	// Use this for initialization
 	void Start () {
-		
+		_player =  GetComponent<Rigidbody2D> ();
 	}
-	
+	 
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKey (KeyCode.RightArrow )) {
+			_player.velocity = new Vector2 (moveSpeed, _player.velocity.y) ;
+		}
+		if(Input.GetKey (KeyCode.LeftArrow )) {
+			_player.velocity = new Vector2 (-moveSpeed, _player.velocity.y) ;
+		}
+		if(Input.GetKeyDown (KeyCode.UpArrow)) {
+			Jump();
+		}
+	}
+
+	public void Jump () {
+		_player.velocity = new Vector2 (_player.velocity.x, jumpPower);
 	}
 }

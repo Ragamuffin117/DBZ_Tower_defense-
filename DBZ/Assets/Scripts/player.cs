8e7b69959668c3ebcb297d228fb3d6d18656e52a
Unknown <andrewgeorge8287@gmail.com>
2018-12-05 22:35:55 +0800

    ok

--- DBZ/Assets/Scripts/player.cs
+++ DBZ/Assets/Scripts/player.cs
@@ -4,26 +4,40 @@ using UnityEngine;
 
 public class player : MonoBehaviour {
 
-	public float moveSpeed;
-	public float jumpPower;
-
+	public float moveSpeed, jumpPower, floorCheckRad;
+	public Transform floorCheck;
+	public LayerMask isFloor;
+	private bool onFloor, jumped;
 	private Rigidbody2D _player;
 	// Use this for initialization
 	void Start () {
 		_player =  GetComponent<Rigidbody2D> ();
 	}
-	 
+
+	void FixedUpdate() {
+		onFloor = Physics2D.OverlapCircle (floorCheck.position, 
+		floorCheckRad, isFloor);
+
+	}
 	// Update is called once per frame
 	void Update () {
+
+		if(onFloor) {
+			jumped = false;
+		}
 		if(Input.GetKey (KeyCode.RightArrow )) {
 			_player.velocity = new Vector2 (moveSpeed, _player.velocity.y) ;
 		}
 		if(Input.GetKey (KeyCode.LeftArrow )) {
 			_player.velocity = new Vector2 (-moveSpeed, _player.velocity.y) ;
 		}
-		if(Input.GetKeyDown (KeyCode.UpArrow)) {
+		if(Input.GetKeyDown (KeyCode.UpArrow) && onFloor) {
 			Jump();
 		}
+		if(Input.GetKeyDown (KeyCode.UpArrow) && !jumped && !onFloor) {
+			Jump();
+		}
+
 	}
 
 	public void Jump () {
